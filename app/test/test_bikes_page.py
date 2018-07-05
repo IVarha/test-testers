e03f31b4a18e29dd156452442d83fc12dfee5a43
igor varga <ivarhauzh@gmail.com>
2018-07-05 23:57:57 +0300

    Add sipmle case

---
+++ app/test/test_bikes_page.py
@@ -0,0 +1,45 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import json
+from pprint import pprint
+
+class BikesPageTests(unittest.TestCase):
+
+	data = []
+	def setUp(self):
+		self.driver = webdriver.Chrome('/Users/igorvarga/Downloads/chromedriver')
+
+		with open('../bikes.json') as f:
+			self.data = json.load(f)
+
+	def test_contains_all_element_sorted(self):
+		driver = self.driver
+		driver.get("http://localhost:8000")
+
+		self.assertEqual('Bike Store' , driver.title)
+		etlems = driver.find_elements_by_class_name('prod')
+
+		self.assertEqual(len(etlems),len(self.data['items']),"did not equal")
+		for i in range(len(etlems)):
+			head = etlems[i].find_element_by_class_name("panel-heading")
+			self.assertEqual(head.text,self.data['items'][i]['name'])
+
+			desc = etlems[i].find_element_by_class_name("desc")
+			self.assertEqual(desc.text,self.data['items'][i]['description'])
+
+			classes = etlems[i].find_element_by_class_name("panel-footer").find_elements_by_class_name('capitalise')
+			self.assertEqual(head.text,self.data['items'][i]['name'])
+			self.assertEqual(len(classes), len(self.data['items'][i]['class']), "did not equal")
+
+			for j in range(len(classes)):
+				self.assertTrue(str(self.data['items'][i]['class'][j]).upper() in str(classes[j].text).upper())
+
+
+
+
+	def tearDown(self):
+		self.driver.close()
+
+if __name__ == "__main__":
+	unittest.main()
